7dd305fd7c6b1d65f775d8118a1d4a89d4d92a1c
Joe618 <zoubh1997@yeah.net>
2025-08-25 03:15:39 +0000

    fix: fix bug

--- data_tools/process_data.py
+++ data_tools/process_data.py
@@ -3,6 +3,8 @@ import os
 import json
 import numpy as np
 import shutil
+from natsort import natsorted
+from tqdm import tqdm
 
 def read_jsonl(file_path):
     data = []
@@ -30,9 +32,9 @@ def write_json(obj, path):
 def copy_a_file(src_path, target_path):
     shutil.copy2(src_path, target_path)
 
-root = "/media/robot/30F73268F87D0FEF/Hil_Serl_Dataset/0820"
-cube_paths = os.listdir(root)
-for cube_path in cube_paths:
+root = "/ssdwork/liujinxin/DATASET/UR5/hub/0820"
+cube_paths = natsorted(os.listdir(root))
+for cube_path in tqdm(cube_paths[1:]):
     # process data.json
     data_json_file_path = os.path.join(root, cube_path, "data.json")
     copy_a_file(data_json_file_path, os.path.join(root, cube_path, "data_bak.json"))
@@ -42,7 +44,7 @@ for cube_path in cube_paths:
         position_prev = np.array(data_list[index-1]["pose"]["position"])   # numpy
         position_true = (position - position_prev).tolist()   # list
         data_list[index-1]["pose"]["position"] = position_true
-    write_json(data_list, data_json_file_path)
+    write_json(data_list[:-1], data_json_file_path)
 
     # process images folders
     images_folder_path = os.path.join(root, cube_path, "image")
@@ -53,13 +55,14 @@ for cube_path in cube_paths:
     for camera in cameras:
         images_folder_path_camera = os.path.join(images_folder_path, camera)
         images = os.listdir(images_folder_path_camera)
-        num_images = len(images) - 1
+        # Why should we -2 here? Because there is a png file in the images.
+        num_images = len(images) - 2
         if camera in ['depth', 'rgb']:
             resolution = '960x540'
         else:
             resolution = '640x480'
         folder_name = f"{camera}_{resolution}_{num_images}"
-        delete_image_path = os.path.join(images_folder_path_camera, folder_name)
+        delete_image_path = os.path.join(images_folder_path_camera, f'{folder_name}.npy')
         if os.path.exists(delete_image_path):
             os.remove(delete_image_path)
         else:
